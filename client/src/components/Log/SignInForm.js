46f8eaf57bd97de77ce3bcff661ddaa20b8107b2
zicokiller <mrichet.cb@gmail.com>
2021-05-29 15:59:37 +0200

    first commit: config app folder and first dependencies. Set login and register functionnality, layouts

---
+++ client/src/components/Log/SignInForm.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const SignInForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const emailError = document.querySelector(".email.error");
+    const passwordError = document.querySelector(".password.error");
+
+    axios({
+      method: "post",
+      url: `${process.env.REACT_APP_API_URL}api/user/login`,
+      withCredentials: true,
+      data: {
+        email,
+        password,
+      },
+    })
+      .then((res) => {
+        if (res.data.errors) {
+          emailError.innerHTML = res.data.errors.email;
+          passwordError.innerHTML = res.data.errors.password;
+        } else {
+          window.location = "/";
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+//   const handleLogin = async e => {
+//     e.preventDefault();
+//     const emailError = document.querySelector(".email.error");
+//     const passwordError = document.querySelector(".password.error");
+//     let res;
+//     try {
+//         res = await axios({
+//             method: "post",
+//             url: `${process.env.REACT_APP_API_URL}api/user/login`,
+//             withCredentials: true,
+//             data: {
+//                 email,
+//                 password,
+//             },
+//         });
+//     } catch (err) {
+//         emailError.innerHTML = err.response.data.email;
+//         passwordError.innerHTML =err.response.data.password;
+//         return console.log(err);
+//     }
+//     window.location = "/";
+// };
+
+  return (
+    <form action="" onSubmit={handleLogin} id="sign-in-form">
+      <label htmlFor="email">Email</label>
+      <br />
+      <input
+        type="text"
+        name="email"
+        value="email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+      <div className="email error"></div>
+      <br />
+      <label htmlFor="password">Mot de passe</label>
+      <br />
+      <input
+        type="password"
+        name="password"
+        value="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+      <div className="password error"></div>
+      <br />
+      <input type="submit" value="Se connecter" />
+    </form>
+  );
+};
+
+export default SignInForm;
